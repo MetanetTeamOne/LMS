f4f2bde954b945cfe2c909b322b0677fb43a91de
maru6889 <crazyboy114@naver.com>
2023-12-15 14:29:01 +0900

    insert add

--- LMS/src/main/java/com/metanetglobal/LMS/student/controller/StudentContorller.java
+++ LMS/src/main/java/com/metanetglobal/LMS/student/controller/StudentContorller.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.metanetglobal.LMS.student.model.StudentDto;
-import com.metanetglobal.LMS.student.model.StudentVO;
+import com.metanetglobal.LMS.student.model.Student;
 import com.metanetglobal.LMS.student.service.IStudentService;
 
 
@@ -41,30 +41,29 @@ public class StudentContorller {
 	@GetMapping("/mypage") //회원정보 조회
 	public StudentDto findStudentById(@RequestBody Map<String, String> map) {
 		String Id = map.get("studentId");
-		System.out.println(Id);
 		StudentDto student = studentService.findStudentById(Id);
-		System.out.println("!!!");
-		System.out.println(student);
 		logger.info("studentId={}", map.get("studentId"));
 		logger.info("student={}", student);
 		return student;
 	}
 	
 	@PostMapping("/signin") //회원가입
-	public String insertStudent(@RequestBody StudentVO student) {
+	public String insertStudent(@RequestBody Student student) {
 		studentService.insertStudent(student);
 		return "ok";
 	}
 	
 	
 	@PatchMapping("/mypage/update")
-	public String updateStudent(@RequestBody StudentVO student){
+	public String updateStudent(@RequestBody Student student){
 		studentService.updateStudent(student);
 		return "ok";
 	}
 	
-	@DeleteMapping("/mypage/delete")
-	public String deleteStudent(@RequestBody String email) {
+	@DeleteMapping("/mypage/delete") //회원 정보 삭제
+	public String deleteStudent(@RequestBody Map<String, String> map) {
+		String email = map.get("email");
+		logger.info("email {}", email);
 		studentService.deleteStudent(email);
 		return "ok";
 	}
